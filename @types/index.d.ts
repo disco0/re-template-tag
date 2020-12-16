8c64fb76b555d4f87cc6b00ccb0954f4edb08b6d
disco0 <disco0@users.noreply.github.com>
2020-12-15 23:59:56 -0800

    Add types build script now inlined w/ cjs build in "build"

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ * @param {TemplateStringsArray} strs
+ * @param {any[]} substs
+ *
+ * @return {RegExp}
+ */
 export function re(strs: TemplateStringsArray, ...substs: any[]): RegExp;
 export function transformRaw(str: string): string;
-export function quoteText(text: any): any;
+export function quoteText(text: string): string;
