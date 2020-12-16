a02ce5596948fd31a22c66a35e4dbcef8eb623e3
disco0 <disco0@users.noreply.github.com>
2020-12-15 20:19:16 -0800

    types

---
+++ @types/index.d.ts
@@ -0,0 +1,3 @@
+export function re(strs: TemplateStringsArray, ...substs: any[]): RegExp;
+export function transformRaw(str: string): string;
+export function quoteText(text: any): any;
--- esm/index.js
+++ esm/index.js
@@ -1,3 +1,9 @@
+/**
+ * @param {TemplateStringsArray} strs
+ * @param {any[]} substs
+ * 
+ * @return {RegExp}
+ */
 export function re(strs, ...substs) {
     let reStr = transformRaw(strs.raw[0]);
     for (const [i, subst] of substs.entries()) {
@@ -6,7 +12,7 @@ export function re(strs, ...substs) {
         } else if (typeof subst === 'string') {
             reStr += quoteText(subst);
         } else {
-            throw new Error('Illegal substitution: '+subst);
+            throw new Error('Illegal substitution: '+ subst);
         }
         reStr += transformRaw(strs.raw[i+1]);
     }
@@ -14,7 +20,7 @@ export function re(strs, ...substs) {
     if (reStr.startsWith('/')) {
         const lastSlashIndex = reStr.lastIndexOf('/');
         if (lastSlashIndex === 0) {
-            throw new Error('If the `re` string starts with a slash, it must end with a second slash and zero or more flags: '+reStr);
+            throw new SyntaxError('If the `re` string starts with a slash, it must end with a second slash and zero or more flags: '+reStr);
         }
         flags = reStr.slice(lastSlashIndex+1);
         reStr = reStr.slice(1, lastSlashIndex);
@@ -22,13 +28,13 @@ export function re(strs, ...substs) {
     return new RegExp(reStr, flags);
 }
 
-function transformRaw(str) {
-    return str.replace(/\\`/g, '`');
-}
+/**
+ * @param  {string} str 
+ * @return {string}
+ */
+export const transformRaw = (str) => str.replace(/\\`/g, '`');
 
 /**
  * All special characters are escaped, because you may want to quote several characters inside parentheses or square brackets.
  */
-export function quoteText(text) {
-    return text.replace(/[\\^$.*+?()[\]{}|=!<>:-]/g, '\\$&');
-}
+export const quoteText = (text) => text.replace(/[\\^$.*+?()[\]{}|=!<>:-]/g, '\\$&');
